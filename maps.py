5ceb2270c0522c1147982831882ffe13ed8acf49
IlyaKull <ilyakull@gmail.com>
2024-05-24 14:27:22 +0200

    gave up on trying to do things without deepcopy

--- maps.py
+++ maps.py
@@ -24,19 +24,18 @@ class Maps:
 	
 	def mod_map(self, sign = None, adjoint = False):
 		'''
-		returns another instance of the  map with modified chosen attributes:
+		returns a copy of the  map with the chosen attributes:
 			sign +/- int 
 			adjoint = true/false
 		'''
 		 
-		 
-		# s = self.class.init(self.__dict__)
+		s = copy.deepcopy(self)
 		
 		# print((self.name, self.adjoint_flag, self.sign))
 		# print((hex(id(self.name)), hex(id(self.adjoint_flag)), hex(id(self.sign))))
 		# print((s.name , s.adjoint_flag, s.sign) )
 		# print((hex(id(s.name)), hex(id(s.adjoint_flag)), hex(id(s.sign))))
-		'''
+		
 		if adjoint != self.adjoint_flag:
 			s.adjoint_flag = adjoint
 			s.dims['in'] = self.dims['out']
@@ -47,21 +46,41 @@ class Maps:
 		
 		if sign:
 			s.sign = sign
-		'''
+		
 		return s
-
+		
+class CGmap(Maps):
+	"""
+	coare-graining map in kraus representation
+	"""
+	def __init__(self, name, sign,
+		representation = None,  # the kraus representation of the map: a tuple of matrices
+		action = {'dimsIn':(), 'pattern':(), 'dimsOut':()} # action pattern: 
+					# dimsIn: tuple  of dimensions of input satate
+					# pattern: 
+							# 0 means Id is acting on that system. 
+							# integer values for blocks on which copies of the same map act:
+							# (0,0,1,1,2,2) means IdxIdxCxC is applied where Id is acting on 1 spin and  C is acting on 2 spins
+					# dimsOut: tuple of output dims
+		):
+				
+		dims = {'in': np.prod(action['dimsIn']), 'out': np.prod(action['dimsOut'])} # total dimensions
+		super().__init__(name, sign, dims, adj_name = name + '^*')
+		self.representation = representation
+		self.action = action
+		
 			
 			
 class TraceWith(Maps):
 	"""
 	maps x -> trace(O*x) ; its adjoint is then c -> c*O
 	"""
-	def __init__(self, name, sign, 
+	def __init__(self, op_name, sign, 
 		operator = 0.0, # the operator with which the trace is taken
-		dim = (), # dimension of input state
-		**kwargs):
-		
-		super().__init__(name, sign, dims = {'in': dim, 'out': 1}, adj_name = name)
+		dim = () # dimension of input state
+		):
+		 		
+		super().__init__(f"{op_name}*", sign, dims = {'in': dim, 'out': 1}, adj_name = op_name)
 		self.operator = operator
 		
 		
@@ -71,10 +90,10 @@ class Identity(Maps):
 	"""
 	maps x -> x 
 	"""
-	def __init__(self,  sign, dim, **kwargs):
-		
+	def __init__(self,  sign, dim):
+				
 		super().__init__('Id', sign, dims = {'in': dim, 'out': dim})
-		self.dim = dim
+		
 			
 
 class Trace(Maps):
@@ -82,11 +101,11 @@ class Trace(Maps):
 	maps x -> trace(O*x) ; its adjoint is then c -> c*O
 	"""
 	def __init__(self,  sign, 
-		dim = (),  # dimension of input state
-		**kwargs):
-		
+		dim = ()  # dimension of input state
+		):
+		self._initial_dim = dim
 		super().__init__('Trace', sign, dims = {'in': dim, 'out': 1}, adj_name = '1*')
-		self.dim = dim
+			
 
 class PartTrace(Maps):
 	"""
@@ -94,37 +113,38 @@ class PartTrace(Maps):
 	"""
 	def __init__(self, sign, 
 		subsystems,  # subsystems to trace : set
-		state_dims, # tuple: dims of input
-		**kwargs):
-		
+		state_dims # tuple: dims of input
+		):
+				
 		name = f"Trace_[{subsystems}/{len(state_dims)}]"
 		remaining_subsystems = {i+1 for i in range(len(state_dims))}.difference(subsystems)
 		dims = {'in': np.prod(state_dims),
 				'out': np.prod( [state_dims[i-1] for i in remaining_subsystems] ) }
 		super().__init__(name, sign, dims, adj_name = f'(x)Id_[{subsystems}/{len(state_dims)}]')
 		self.subsystems = subsystems
-		self.state_dims = state_dims
 		
 		
-		
-class CGmap(Maps):
+class MPSmap(CGmap):
 	"""
-	coare-graining map in kraus representation
-	"""
-	def __init__(self, name, sign,
-		representation = None,  # the kraus representation of the map: a tuple of matrices
-		action = {'dimsIn':(), 'pattern':(), 'dimsOut':()}, # action pattern: 
-					# dimsIn: tuple  of dimensions of input satate
-					# pattern: 
-							# 0 means Id is acting on that system. 
-							# integer values for blocks on which copies of the same map act:
-							# (0,0,1,1,2,2) means IdxIdxCxC is applied where Id is acting on 1 spin and  C is acting on 2 spins
-					# dimsOut: tuple of output dims
-		**kwargs):
+	Represnts a coarse-graining map in the MPS-based C-G scheme.
+	Such maps are used in in the ground-state enrgy (GSE) problem and in the gap certification problem (GAP).
+	In GSE the maps act on the leftmost or rightmost m spins of a state, i.e. either as IxW (right version) or as WxI (left version)
+	In GAP there are two additional left untouched by the map, one on each side of the state. The same map W act in this setting as IxWxII (left) or  IIxWxI (right).
+	"""	
+	
+	def __init__(self,
+		representation, acts_on, sign, # as in parent class "CGmap"
+		id_pad # the number of identity terms padding the map  (0 for GSE, 1 for GAP)
+		):
+		super().__init__(representation, acts_on)
+		self.id_pad = id_pad 
 		
-		dims = {'in': np.prod(action['dimsIn']), 'out': np.prod(action['dimsOut'])} # total dimensions
-		super().__init__(name, sign, dims, adj_name = name + '^*')
-		self.representation = representation
-		self.action = action
 		
+	def apply(self, l_r, adj, var):
+		"""
+		apply map to variable
+		l_r specifies left or right version
+		adj specifies adjoint
+		"""
+		return 
 		
