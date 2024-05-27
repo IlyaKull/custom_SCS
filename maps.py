c93eac0755b59157d4d106022466328b73d6df45
IlyaKull <ilyakull@gmail.com>
2024-05-27 17:50:57 +0200

    need to make __apply__() method for maps

--- maps.py
+++ maps.py
@@ -2,7 +2,7 @@
 import numpy as np
 import copy
 
-from matrix_aux_functions import vec2symm
+from matrix_aux_functions import vec2mat
  
 class Maps:
 	"""
@@ -46,31 +46,25 @@ class Maps:
 	def __call__(self, in_var, out_var, in_vec, out_vec):
 		""" 
 		applies the map to the components of in_vec contatining in_var from the list of variables 
-		and assigns the result to the components of out_vec contatining out_var. 
-		e.g.
-		constraint  = trace(rho*h) - 1 = 0 with conj bariable alpha
-		and in_vec = [rho, sigma, omega], out_vec = [alpha, beta]
-		then
-		TraceWith: rho --> trace(h*rho) --> alpha component of out_vec
 		"""
 		
-
 		
 		assert (in_var.primal_or_dual == 'primal' and out_var.primal_or_dual == 'dual') or \
-				(in_var.primal_or_dual == 'dual' and out_var.primal_or_dual == 'primal'), \  
-				('in_var and out_var sould be a prima-dual pair\n.' + f'in_var {in_var.name} is {in_var.primal_or_dual}\n' + 
-				f'out_var {out_var.name} in {out_var.primal_or_dual}' )
-			)
+				(in_var.primal_or_dual == 'dual' and out_var.primal_or_dual == 'primal'),  \
+				('in_var and out_var sould be a prima-dual pair\n.' + f'in_var {in_var.name} is {in_var.primal_or_dual}\n' + \
+				f'out_var {out_var.name} in {out_var.primal_or_dual}' ) 
+			
 		
-		
-		if np.prod(in_var.dims) == 1:
-			matrix_var = vec2symm(dim = 1 , in_vec[in_var.indices[0]]) # real scalar
-		else if in_var.complex:
-			matrix_var = vec2symm(dim = np.prod(in_var.dims) ,  in_vec[in_var.indices[0]:in_var.indices[1]])
+		 
+		if in_var.complex:
+			matrix_var = vec2mat(dim = np.prod(in_var.dims), real_part = in_vec[in_var.indices_real[0] : in_var.indices_real[0]+1 ], \
+				imag_part = in_vec[in_var.indices_imag[0] : in_var.indices_imag[0]+1 ]		)
 		else:
-			matrix_var = vec2symm(dim = np.prod(in_var.dims) ,  real_part = in_vec[in_var.indices[0]:in_var.indices[1]],
-				imag_part =in_vec[in_var.indices[2]:in_var.indices[3]]
-			)
+			matrix_var = vec2mat(dim = np.prod(in_var.dims), real_part = in_vec[in_var.indices_real[0] : in_var.indices_real[0]+1 ] )
+		
+		return self.__apply__(matrix_var)
+		
+		
 		
 		
 		
--- variables.py
+++ variables.py
@@ -31,18 +31,26 @@ class OptVar:
 			if not var_list:
 				last_index = -1
 			else:
-				last_index = var_list[-1].indices[-1] 
+				if var_list[-1].complex:
+					last_index = var_list[-1].indices_imag[-1] 
+				else:
+					last_index = var_list[-1].indices_real[-1] 
 			
 			if np.prod(dims)==1:
-				self.indices = [last_index +1]
+				self.indices_real = [last_index +1, last_index +1]
+				if self.complex:
+					print(f'!!!!!!!!!!!!!! variable {self.name} was set as complex but has dim=1 -->> set to be real')
+					self.complex = False
+				
 			else:
 				 # inds for real part
-				self.indices = [last_index +1, last_index + dim_symm_matrix(np.prod(dims)) ]
+				self.indices_real = [last_index +1, last_index + dim_symm_matrix(np.prod(dims)) ]
 				 # inds for imaginary part
 				if self.complex:
-					last_index = self.indices[-1]
-					self.indices += [last_index +1, last_index + dim_AntiSymm_matrix(np.prod(dims))]
-					
+					last_index = self.indices_real[-1]
+					self.indices_imag = [last_index +1, last_index + dim_AntiSymm_matrix(np.prod(dims))]
+				else:
+					self.indices_imag = None
 					
 					
 			
@@ -50,7 +58,24 @@ class OptVar:
 			var_list.append(self)			
 			
 			print(f"{complex_or_real[complex_var]} variable {self.name} was added to the list of {primal_or_dual} variables")
-			print(self.indices)
+			
+	
+	# def get_inds(self):
+		# '''
+		# retriev indices for real and imaginary part
+		# '''
+		
+		# if np.prod(self.dims) == 1:
+			# real_inds = [in_var.indices[0] , in_var.indices[0]+1]
+		# else:
+			# real_inds = [in_var.indices[0], in_var.indices[1]]
+		
+		# if self.complex:
+			# imag_inds = [in_var.indices[2]:in_var.indices[3]]
+		# else:
+			# imag_inds = None
+		# return (real_inds, imag_inds)
+	
 				
 	def print_var_list(self):
 		print('='*80)
@@ -60,10 +85,10 @@ class OptVar:
 		for i, var_list in enumerate((OptVar.primal_vars, OptVar.dual_vars)):
 			print('~'*10 + " {} VARS:".format(('PRIMAL', 'DUAL')[i]))
 			for var in var_list:
+				print(f"{var.name:20} Inds real: {var.indices_real}", end=' ')
 				if var.complex:
-					print(f"{var.name:20} Inds: real, imag: {var.indices}", end=' ')
-				else:
-					print(f"{var.name:20} Inds: real: {var.indices}", end=' ')
+					print(f"{var.name:20} Inds_imag: {var.indices_imag}", end=' ')
+								
 				print(f": dims: {var.dims}: {complex_or_real[var.complex]}")
 	
 
