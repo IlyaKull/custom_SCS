98f807e19d87daa7bdb897e706f61b9dbbd6f8eb
IlyaKull <ilyakull@gmail.com>
2025-04-01 14:58:02 +0200

    fixing map calls logging

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import line_profiler
 import sys, os
-
+import numpy as np
 import logging
 logger = logging.getLogger()
 logging.addLevelName(logging.DEBUG, '...DEBUG') 
 logging.addLevelName(5,"verbose")
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(levelname)-8s: %(name)-20s:: %(message)s', datefmt='%H:%M:%S')
-# logger.setLevel(logging.DEBUG)
+ 
 
-import numpy as np
-
-import scs_funcs
-# from  variables import OptVar
-from scipy.sparse.linalg import LinearOperator
-from maps import Maps
-# import constraints
-import matrix_aux_functions as mf
-import LTI_N_problem, relax_LTI_N_problem, one_step_relax_LTI_N_problem, GAP_LTI_N_problem
 
 import default_settings
 
+
+
+# choose which problem to run
+# problem specific command line arguments are then defined in the problem module
+
+### LTI problem for heisenberg model
+# import LTI_N_problem
 # problem_module = LTI_N_problem
+
+### mps relaxation of LTI problem heisenberg model
+import relax_LTI_N_problem
 problem_module = relax_LTI_N_problem 
+
+### gap LTI SDP for AKLT model
+# import GAP_LTI_N_problem
 # problem_module = GAP_LTI_N_problem
 
 
@@ -29,30 +33,8 @@ problem_module = relax_LTI_N_problem
 
 def main():
 		
-	# define problem specific command-line arguments
-	parser = problem_module.define_arguments()
 	
-	# add general purpose command line arguments
-	parser.add_argument("--verbose", help="increase output verbosity",
-						action="store_true")
-	parser.add_argument("--profileLines", help="line profiler",
-						action="store_true")
-	parser.add_argument("--workers", help="number of workers", 
-					type=int, default = 1)
-	parser.add_argument("--OMP_NUM_THREADS", help="OMP_NUM_THREADS evn var", 
-					type=int, default = 1)
-	parser.add_argument("--maxiter", help="maximum number of SCS iterations", 
-					type=int, default = 2000)
-	parser.add_argument("--dispiters", help="display every x iterations", 
-					type=int, default = 100)
-	parser.add_argument("--scs_scaling_sigma", help="SCS parameter that rescales b", 
-					type=float, default = 0.001)
-	parser.add_argument("--scs_scaling_rho", help="SCS parameter that rescales c", 
-					type=float, default = 1.0)
-	parser.add_argument("--scs_adapt_scale_if_ratio", help="adaptive rescalint if primal to dual residual ratio exceeds this amount", 
-					type=float, default = 50.0)
-
-	
+	parser = make_parser()
 					
 	args = parser.parse_args()
 	
@@ -66,8 +48,13 @@ def main():
 
 	if args.profileLines:
 		profile = line_profiler.LineProfiler()
-		profile.add_function(scs_funcs._impl_apply_constr)
-		profile.add_function(scs_funcs.SCS_Solver._project_to_cone)
+		
+		# import constraints
+		# profile.add_function(constraints.Constraint.__call__)
+		
+		import maps
+		profile.add_function(maps.Maps.__call__)
+		
 		profile.enable_by_count()
 
 	
@@ -101,6 +88,36 @@ def main():
 	if args.profileLines:
 		profile.print_stats()
 	
+	
+	
+	
+
+def make_parser():
+	# define problem specific command-line arguments
+	parser = problem_module.define_arguments()
+	
+	# add general purpose command line arguments
+	parser.add_argument("--verbose", help="increase output verbosity",
+						action="store_true")
+	parser.add_argument("--profileLines", help="line profiler",
+						action="store_true")
+	parser.add_argument("--workers", help="number of workers", 
+					type=int, default = 1)
+	parser.add_argument("--OMP_NUM_THREADS", help="OMP_NUM_THREADS evn var", 
+					type=int, default = 20)
+	parser.add_argument("--maxiter", help="maximum number of SCS iterations", 
+					type=int, default = 2000)
+	parser.add_argument("--dispiters", help="display every x iterations", 
+					type=int, default = 100)
+	parser.add_argument("--scs_scaling_sigma", help="SCS parameter that rescales b", 
+					type=float, default = 0.001)
+	parser.add_argument("--scs_scaling_rho", help="SCS parameter that rescales c", 
+					type=float, default = 1.0)
+	parser.add_argument("--scs_adapt_scale_if_ratio", help="adaptive rescalint if primal to dual residual ratio exceeds this amount", 
+					type=float, default = 50.0)
+					
+	return parser
+
 	
 if __name__ == '__main__':
 	main()
